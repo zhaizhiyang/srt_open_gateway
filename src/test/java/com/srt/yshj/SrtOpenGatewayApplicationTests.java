5c86c35c71a049f0585b637f1fd011ce9fa78a98
qinhuiying <qinhuiying@everjiankang.com>
2020-12-05 18:20:17 +0800

    首次提交

---
+++ src/test/java/com/srt/yshj/SrtOpenGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srt.yshj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtOpenGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
